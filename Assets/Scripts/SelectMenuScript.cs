f3e1b732d9faf26931dfb7231c0b831501719591
LipeGabriell <felipe.ramiressanches@hotmail.com>
2023-07-22 09:50:35 -0300

    aadd histórias e primeiro quiz (agentes)

--- Assets/Scripts/SelectMenuScript.cs
+++ Assets/Scripts/SelectMenuScript.cs
@@ -5,45 +5,129 @@ using UnityEngine.SceneManagement;
 
 public class SelectMenuScript : MonoBehaviour
 {
-    private void Start()
+    [SerializeField] private string anteriorScreen;
+
+    private const string Inicio = "Assets/Scenes/Inicio.unity";
+
+    private const string HistoriasLobby = "Assets/Scenes/Historias/Historias.unity";
+    private const string AgentesHistoria = "Assets/Scenes/Historias/Cenas/AgentesCausadoresHistoria.unity";
+    private const string QueimaduraHistoria = "Assets/Scenes/Historias/Cenas/GrausQueimaduraHistoria.unity";
+    private const string PrevençoesHistoria = "Assets/Scenes/Historias/Cenas/PrevençoesHistoria.unity";
+    private const string PrimeirosHistoria = "Assets/Scenes/Historias/Cenas/PrimeirosSocorrosHistoria.unity";
+
+    private const string QuizLobby = "Assets/Scenes/Quiz/Quiz.unity";
+
+    
+    private const string Creditos = "Assets/Scenes/Creditos.unity";
+
+    private const string Devs = "Assets/Scenes/Devs.unity";
+
+    private const string References = "Assets/Scenes/referencias.unity";
+
+    private const string Ensino = "Assets/Scenes/Ensino.unity";
+
+
+    private void ChangeScreen(string path)
     {
-        DontDestroyOnLoad(this);
+        var sceneLoading = SceneManager.LoadSceneAsync(path);
+        var cena = SceneManager.GetSceneByPath(path);
+        sceneLoading.completed += operation => SceneManager.SetActiveScene(cena);
+    }
+
+    public void ReturnAnteriorScreen()
+    {
+        var sceneLoading = SceneManager.LoadSceneAsync(anteriorScreen);
+        var cena = SceneManager.GetSceneByPath(anteriorScreen);
+        sceneLoading.completed += operation => SceneManager.SetActiveScene(cena);
     }
 
     public void SelectMenu()
     {
-        const string path  = "Assets/Scenes/Inicio.unity";
-        var sceneLoading = SceneManager.LoadSceneAsync(path);
-        var cena = SceneManager.GetSceneByPath(path);
-        SceneManager.SetActiveScene(cena);
+        anteriorScreen = Inicio;
+        ChangeScreen(Inicio);
     }
 
     public void SelectHistories()
     {
-        const string path  = "Assets/Scenes/Historias.unity";
-        var sceneLoading = SceneManager.LoadSceneAsync(path);
-        var cena = SceneManager.GetSceneByPath(path);
-        SceneManager.SetActiveScene(cena);
+        anteriorScreen = Inicio;
+        ChangeScreen(HistoriasLobby);
+    }
+
+    public void HistoriaAgentes()
+    {
+        anteriorScreen = HistoriasLobby;
+        ChangeScreen(AgentesHistoria);
+    }
+
+    public void HistoriaGraus()
+    {
+        anteriorScreen = HistoriasLobby;
+        ChangeScreen(QueimaduraHistoria);
+    }
+
+    public void HistoriaPrevencoes()
+    {
+        anteriorScreen = HistoriasLobby;
+        ChangeScreen(PrevençoesHistoria);
+    }
+
+    public void HistoriaPrimeiros()
+    {
+        anteriorScreen = HistoriasLobby;
+        ChangeScreen(PrimeirosHistoria);
     }
 
     public void SelectQuiz()
     {
-        
-        const string path  = "Assets/Scenes/Quiz.unity";
-        var sceneLoading = SceneManager.LoadSceneAsync(path);
-        var cena = SceneManager.GetSceneByPath(path);
-        SceneManager.SetActiveScene(cena);
-
+        anteriorScreen = Inicio;
+        ChangeScreen(QuizLobby);
     }
 
+    public void QuizAgentes()
+    {
+        anteriorScreen = QuizLobby;
+        ChangeScreen(AgentesHistoria);
+    }
+
+    public void QuizGraus()
+    {
+        anteriorScreen = QuizLobby;
+        ChangeScreen(QueimaduraHistoria);
+    }
+
+    public void QuizPrevencoes()
+    {
+        anteriorScreen = QuizLobby;
+        ChangeScreen(PrevençoesHistoria);
+    }
+
+    public void QuizPrimeiros()
+    {
+        anteriorScreen = QuizLobby;
+        ChangeScreen(PrimeirosHistoria);
+    }
+    
     public void SelectCredits()
     {
-        const string path  = "Assets/Scenes/Creditos.unity";
-        var sceneLoading = SceneManager.LoadSceneAsync(path);
-        var cena = SceneManager.GetSceneByPath(path);
-        SceneManager.SetActiveScene(cena);
+        anteriorScreen = Inicio;
+        ChangeScreen(Creditos);
     }
 
+    public void SelectDevs()
+    {
+        anteriorScreen = Creditos;
+        ChangeScreen(Devs);
+    }
 
-   
+    public void SelectReferences()
+    {
+        anteriorScreen = Creditos;
+        ChangeScreen(References);
+    }
+
+    public void SelectEnsino()
+    {
+        anteriorScreen = Creditos;
+        ChangeScreen(Ensino);
+    }
 }
